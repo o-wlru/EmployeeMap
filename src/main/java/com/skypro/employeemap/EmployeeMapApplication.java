19cfb984061fef7cac7199d2f93c2a9120718e31
owlru <valujenich@inbox.ru>
2022-03-21 13:55:03 +0300

    Первый коммит

---
+++ src/main/java/com/skypro/employeemap/EmployeeMapApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.employeemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeMapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skypro/employeemap/EmployeeMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skypro.employeemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
